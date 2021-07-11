0560f9002cf615aca54ae05597114cb9e80debb8
yangjunwei <18438049166@163.com>
2021-07-11 16:58:40 +0800

    # 提交线程池的执行总结

--- docs/.vuepress/configs/sidebar/concurrent.js
+++ docs/.vuepress/configs/sidebar/concurrent.js
@@ -12,9 +12,9 @@ module.exports = [
         "collapsable": true,
         "children": [
             "/java/concurrent/threadpool/线程池概述",
-            "/java/concurrent/threadpool/线程池的调度过程",
             "/java/concurrent/threadpool/线程池的创建过程",
             "/java/concurrent/threadpool/线程池的状态",
+            "/java/concurrent/threadpool/线程池的执行",
             "/java/concurrent/threadpool/线程池的关闭",
             "/java/concurrent/threadpool/JDK提供的线程池"
         ]
