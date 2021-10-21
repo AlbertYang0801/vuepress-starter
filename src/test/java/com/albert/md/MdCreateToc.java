6f04132cbe93943df0ccfe37ca146c737f7772f8
yangjunwei <18438049166@163.com>
2021-10-21 16:37:33 +0800

    # 提交MD增加目录的方法

---
+++ src/test/java/com/albert/md/MdCreateToc.java
@@ -0,0 +1,23 @@
+package com.albert.md;
+
+import com.github.houbb.markdown.toc.core.impl.AtxMarkdownToc;
+import com.github.houbb.markdown.toc.vo.TocGen;
+
+/**
+ * 为指定MD文件生成目录
+ * @author Albert
+ * @date 2021/1/25 下午5:30
+ */
+public class MdCreateToc {
+
+    public static void main(String[] args) {
+
+        String macPath="/Users/yangjunwei/vuepress-starter/src/test/java/com/albert/md/READMEBak.md";
+//        String windowsPath="D:\\IdeaWorkSpace\\concurrent-practice\\src\\test\\java\\com\\albert\\concurrent\\md\\READMEBak.md";
+        TocGen tocGen = AtxMarkdownToc.newInstance()
+                .genTocFile(macPath);
+        System.out.println(tocGen);
+    }
+
+
+}
